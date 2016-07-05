52f6459ef74b4f42c81288ed51e30a1e1fa2b215
Lucas Ortigara Reis <lucas.reis@softexpert.com>
2016-07-05 13:26:54 -0300

    Componente proprio (template)+ diretivas

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,11 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 
 	$scope.fotos = [];
-    $http.get('v1/fotos').success(function(retorno) {
-  		$scope.fotos = retorno.data;
+	$http.get('/v1/fotos').success(function(retorno) {
+			console.log(retorno);
+  		$scope.fotos = retorno;
 	}).error(function(erro) {
-		console.log('Fotos não encontradas!!!');
+		console.log('Fotos não encontradas:: '.erro);
 	});
 
 });
