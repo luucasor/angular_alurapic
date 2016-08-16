baa80210f0f445401bfffc58397c23f9fc422c3a
luucasor <luucasor@gmail.com>
2016-08-16 13:14:47 -0300

    Inserida diretiva ng-model-options com a propriedade debounce: 500

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,12 +3,12 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	$scope.fotos = [];
 	$scope.filtro = '';
 
-	$http.get('/v1/fotos').success(function(retorno) {
-			console.log(retorno);
+	$http.get('/v1/fotos')
+	.success(function(retorno) {
   		$scope.fotos = retorno;
-	}).error(function(erro) {
+	})
+	.error(function(erro) {
 		console.log('Fotos não encontradas:: '.erro);
 	});
-
-	console.log($scope.filtro);
+	
 });
