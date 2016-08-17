bb24fea260ce34026d532dd3ae00e0a9a612bcec
luucasor <luucasor@gmail.com>
2016-08-17 13:56:29 -0300

    adicionado sistema para SPA (Single Page Application)

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -9,4 +9,16 @@ angular.module('minhasDiretivas', [])
     ddo.transclude = true;
     ddo.templateUrl = "js/directives/meu-painel.html";
     return ddo;
-});
+})
+.directive('minhaFoto', function(){
+    var ddo = {};
+
+    ddo.restric = "AE";
+    ddo.scope = {
+      titulo: '@'
+    }
+    ddo.transclude = true;
+    ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
+    return ddo;
+})
+;
